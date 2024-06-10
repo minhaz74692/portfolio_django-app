f854c9df7d613bd3df7cc61cd8ca7a0fb7fe8a90
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-06-10 23:26:08 +0600

    added models

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userId = models.IntegerField()
+    userName  = models.CharField(max_length= 24)
+    email = models.CharField(max_length=30)
+    age  = models.IntegerField()
+    profession = models.CharField(max_length= 20)
